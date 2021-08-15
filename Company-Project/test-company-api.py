5a7f1217f42c8e9cc4303a13feb983d2390c8855
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-08-15 17:32:49 +0200

    changes

--- Company-Project/test-company-api.py
+++ Company-Project/test-company-api.py
@@ -3,7 +3,9 @@ import json
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "/c/")
+response = requests.get(BASE + "/getcompanies/")
+print(f"STATUS CODE: {response.status_code}")
+print(f"CONTENT-TYPE: {response.headers['Content-Type']}")
 print(f"DATA: {response.json()}")
 with open("companies-data-serialize.json","w") as json_file:
     json.dump(response.json(),json_file)
