dcfab4006a856243aa994ed7d210eca826ae84e5
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-10-01 17:03:25 +0200

    Update tempmysql.sql

--- tempmysql.sql
+++ tempmysql.sql
@@ -1,7 +1,13 @@
 SELECT * FROM sd_db.salesdata;
 -- Data Manipulation
+SET SQL_SAFE_UPDATES = 0;
 DELETE FROM sd_db.salesdata;
 UPDATE sd_db.salesdata SET sd_db.salesdata.MiddleName = 'Unknown' WHERE sd_db.salesdata.MiddleName IS NULL;
+UPDATE sd_db.salesdata SET sd_db.salesdata.Title = 'Unknown' WHERE sd_db.salesdata.Title IS NULL;
+UPDATE sd_db.salesdata SET sd_db.salesdata.Gender = 'Unknown' WHERE sd_db.salesdata.Gender IS NULL;
+
+
+
 
 -- Completeness
 UPDATE sd_db.salesdata SET sd_db.salesdata.FirstName = '' WHERE sd_db.salesdata.MiddleName IS NULL AND sd_db.salesdata.Gender = 'Female';
@@ -17,3 +23,7 @@ SET sd_db.salesdata.EmailAddress =
  concat(substring(sd_db.salesdata.Address,1,4), sd_db.salesdata.LastName,'.net')
 WHERE sd_db.salesdata.PersonID < 465;
 
+
+-- Getting the valid addresses only
+INSERT INTO sd_db.valid_addresses (address)
+SELECT sd_db.salesdata.Address FROM sd_db.salesdata WHERE sd_db.salesdata.Address REGEXP '.*\\d.*';
